aeb60599bb791d8dde90c3840660bf0802051144
MakSICK1287 <pitalenkomaksim@gmail.com>
2026-07-14 15:23:24 +0300

    Added Confitmation Alerts Test

--- SeleniumFreeCodeCamp/src/main/java/com/demoqa/pages/AlertsFramesWindows/AlertsPage.java
+++ SeleniumFreeCodeCamp/src/main/java/com/demoqa/pages/AlertsFramesWindows/AlertsPage.java
@@ -7,17 +7,25 @@ public class AlertsPage extends AlertsFrameWindowPage {
     private By alertButton = By.id("alertButton");
     private By timerAlertButton = By.id("timerAlertButton");
     private By confirmButton = By.id("confirmButton");
+    private By confirmResult = By.id("confirmResult");
     private By promtButton = By.id("promtButton");
 
     public void clickAlertButton(){
         click(alertButton);
     }
+
     public void clickTimerAlertButton(){
         click(timerAlertButton);
     }
+
     public void clickConfirmButton(){
         click(confirmButton);
     }
+
+    public String getConfirmText(){
+        return find(confirmResult).getText();
+    }
+
     public void clickPromptButton(){
         click(promtButton);
     }
--- SeleniumFreeCodeCamp/src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
+++ SeleniumFreeCodeCamp/src/test/java/part3_4/com/demoqa/tests/part4/alerts/AlertsTest.java
@@ -5,15 +5,21 @@ import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
 import static utilities.SwitchToUtility.*;
-
+@Test
 public class AlertsTest extends BaseTest {
 
-    @Test
     public void testInformationAlert(){
         var alertsPage = homePage.goToAFWPage().clickAlertsMenu();
         String expectedAlertText = "You clicked a button";
         alertsPage.clickAlertButton();
         Assert.assertEquals(getAlertText(),expectedAlertText, "\n Actual and Expected Messages DO NOT Match \n");
         acceptAlert();
+    }
+    public void testConfirmationAlert(){
+        var alertsPage = homePage.goToAFWPage().clickAlertsMenu();
+        alertsPage.clickConfirmButton();
+        dismissAlert();
+        String actualResult = alertsPage.getConfirmText();
+        Assert.assertTrue(actualResult.contains("Cancel"), "Actual Result Does NOT Contain 'Cancel'");
     }
 }
